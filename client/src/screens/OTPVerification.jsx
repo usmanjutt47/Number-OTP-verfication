3f32687502c515ab19bfdd753de27e09e74ac6fe
Fahadayyaz <62334285+Fahadayyaz@users.noreply.github.com>
2024-08-05 12:23:28 +0500

    Update OTPVerification.jsx

--- client/src/screens/OTPVerification.jsx
+++ client/src/screens/OTPVerification.jsx
@@ -64,7 +64,7 @@ const styles = StyleSheet.create({
   },
   contentContainer: {
     flex: 1,
-    width: "85%",
+    width: "95%",
     // backgroundColor: "lightblue",
     alignSelf: "center",
   },

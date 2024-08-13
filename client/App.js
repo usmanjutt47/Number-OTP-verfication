719a89df3deb9b4008736bcec4517d30ca90c7ff
Fahadayyaz <62334285+Fahadayyaz@users.noreply.github.com>
2024-08-13 11:19:29 +0500

    Revert "Updated App.js"
    This reverts commit de96a0fde725cfb0b25456c564e8b255dd6a2056.

--- client/App.js
+++ client/App.js
@@ -8,10 +8,6 @@ import OnBoarding from "./screens/OnBoarding";
 import EmailVerification from "./screens/EmailVerification";
 import OTPVerification from "./screens/OTPVerification";
 import Home from "./screens/Home";
-import Profile from "./screens/Profile";
-import Favorite from "./screens/Favorite";
-import AllChats from "./screens/AllChats";
-import CustomTopNav from "./components/CustomTopNav";
 
 const Stack = createStackNavigator();
 
@@ -44,10 +40,8 @@ const App = () => {
   useEffect(() => {
     const checkUser = async () => {
       const userId = await AsyncStorage.getItem("userId");
+      console.log("User ID from AsyncStorage:", userId);
       if (userId) {
-        {
-          /* home added as initial rout */
-        }
         setInitialRoute("Home");
       } else {
         setInitialRoute("OnBoarding");
@@ -72,10 +66,6 @@ const App = () => {
         <Stack.Screen name="OTPVerification" component={OTPVerification} />
         <Stack.Screen name="OnBoarding" component={OnBoarding} />
         <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="Profile" component={Profile} />
-        <Stack.Screen name="Favorite" component={Favorite} />
-        <Stack.Screen name="AllChats" component={AllChats} />
-        <Stack.Screen name="CustomTopNav" component={CustomTopNav} />
       </Stack.Navigator>
     </NavigationContainer>
   );

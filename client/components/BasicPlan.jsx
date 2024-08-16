4dd450f8e8adcd0e40d2942f42225d1c3423da60
Fahadayyaz <62334285+Fahadayyaz@users.noreply.github.com>
2024-08-16 10:30:04 +0500

    BasicPlan done

--- client/components/BasicPlan.jsx
+++ client/components/BasicPlan.jsx
@@ -3,6 +3,10 @@ import React, { useState } from "react";
 import Ionicons from "@expo/vector-icons/Ionicons";
 
 const BasicPlan = ({ isButtonPressed, handleButtonPress }) => {
+  {
+    /* all colors are here */
+  }
+
   const colors = {
     primary: "#075856",
     darkGray: "#8B8E97",

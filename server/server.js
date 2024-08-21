6661005101235c545e995ef672ba516bb29d319f
Usman Jutt <142773789+usmanjutt47@users.noreply.github.com>
2024-08-21 01:35:48 -0700

    delete

--- server/server.js
+++ server/server.js
@@ -6,7 +6,6 @@ const morgan = require("morgan");
 const Stripe = require("stripe");
 const connectDataBase = require("./config/DataBase");
 const userRoutes = require("../server/routes/userRoutes");
-const cron = require("node-cron");
 const User = require("./models/userModel"); // Adjust path based on your project structure
 
 // Load environment variables
@@ -21,15 +20,6 @@ app.use(express.json());
 app.use(morgan("dev"));
 
 app.use("/api/v1/auth", userRoutes);
-
-cron.schedule("0 0 * * *", async () => {
-  try {
-    await User.updateMany({}, { $set: { lettersCreatedToday: 0 } });
-    console.log("Daily reset of lettersCreatedToday count completed.");
-  } catch (error) {
-    console.error("Error during daily reset:", error);
-  }
-});
 
 // Start the server
 const PORT = process.env.PORT || 3000;
